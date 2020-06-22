a251cfe86dd79280b9e91041b3e41995dbb9a93e
aticu <15schnic@gmail.com>
2020-06-22 11:41:52 +0200

    Move const generics types one module up
    This results in shorter and therefore hopefully more readable error
    messages on nightly.

--- main/src/const_generics_types.rs
+++
@@ -1,7 +0,0 @@
-//! Contains the types needed for the const generics implementation.
-
-/// A declaration that a custom condition holds.
-pub struct CustomConditionHolds<const CONDITION: &'static str>;
-
-/// A declaration that the pointer of name `PTR` is valid.
-pub struct ValidPtrConditionHolds<const PTR: &'static str, const ACCESS_TYPE: &'static str>;
