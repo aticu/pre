aticu/pre

commit 39cfecf1d295a019d715cd384269f45c2dd53bd8

Improve spans for missing preconditions

proc-macro/src/const_generics_impl.rs
@@ -101,6 +101,15 @@ pub(crate) fn render_pre(
 ) -> TokenStream {
     let preconditions = render_condition_list(preconditions, span);
 
+    // Include the precondition site into the span of the function.
+    // This improves the error messages for the case where no preconditions are specified.
+    function.sig.fn_token.span = function
+        .sig
+        .fn_token
+        .span
+        .join(span)
+        .unwrap_or_else(|| span);
+
     function.sig.inputs.push(
         parse2(quote_spanned! { span=>
             #[cfg(not(doc))]

proc-macro/src/struct_impl.rs
@@ -136,6 +136,15 @@ pub(crate) fn render_pre(
         }
     };
 
+    // Include the precondition site into the span of the function.
+    // This improves the error messages for the case where no preconditions are specified.
+    function.sig.fn_token.span = function
+        .sig
+        .fn_token
+        .span
+        .join(span)
+        .unwrap_or_else(|| span);
+
     function.sig.inputs.push(
         parse2(quote_spanned! { span=>
             #[cfg(not(doc))]

tests/nightly/function/compile_fail/missing_assure.stderr
@@ -1,10 +1,12 @@
 error[E0061]: this function takes 1 argument but 0 arguments were supplied
  --> $DIR/missing_assure.rs:8:5
   |
-4 | fn foo() {}
-  | -------- defined here
+3 |   #[pre("must be bar")]
+  |  _______-
+4 | | fn foo() {}
+  | |___________- defined here
 ...
-8 |     foo()
-  |     ^^^-- supplied 0 arguments
-  |     |
-  |     expected 1 argument
+8 |       foo()
+  |       ^^^-- supplied 0 arguments
+  |       |
+  |       expected 1 argument

tests/stable/function/compile_fail/missing_assure.stderr
@@ -1,10 +1,12 @@
 error[E0061]: this function takes 1 argument but 0 arguments were supplied
  --> $DIR/missing_assure.rs:8:5
   |
-4 | fn foo() {}
-  | -------- defined here
+3 |   #[pre("must be bar")]
+  |  _______-
+4 | | fn foo() {}
+  | |___________- defined here
 ...
-8 |     foo()
-  |     ^^^-- supplied 0 arguments
-  |     |
-  |     expected 1 argument
+8 |       foo()
+  |       ^^^-- supplied 0 arguments
+  |       |
+  |       expected 1 argument

proc-macro/src/pre_attr.rs
@@ -144,6 +144,12 @@ impl VisitMut for PreAttrVisitor {
 
 /// Renders the given function and applies all `pre` attributes to it.
 fn render_function(function: &mut ItemFn, first_attr: Option<PreAttr>) -> TokenStream {
+    let first_attr_span = first_attr.as_ref().and_then(|attr| match attr {
+        PreAttr::Empty => None,
+        PreAttr::NoDoc(no_doc) => Some(no_doc.span()),
+        PreAttr::Precondition(precondition) => Some(precondition.span()),
+    });
+
     let mut preconditions: Vec<_> = first_attr
         .and_then(|attr| match attr {
             PreAttr::Precondition(precondition) => Some(precondition),
@@ -164,6 +170,15 @@ fn render_function(function: &mut ItemFn, first_attr: Option<PreAttr>) -> TokenS
         },
     );
 
+    let span = match (attr_span, first_attr_span) {
+        (Some(attr_span), Some(first_attr_span)) => {
+            attr_span.join(first_attr_span).unwrap_or_else(|| attr_span)
+        }
+        (Some(span), None) => span,
+        (None, Some(span)) => span,
+        (None, None) => Span::call_site(), // Should never be the case for non-empty preconditions
+    };
+
     if !preconditions.is_empty() {
         if render_docs {
             function
@@ -171,11 +186,7 @@ fn render_function(function: &mut ItemFn, first_attr: Option<PreAttr>) -> TokenS
                 .push(generate_docs(&function.sig, &preconditions, None));
         }
 
-        render_pre(
-            preconditions,
-            function,
-            attr_span.unwrap_or_else(Span::call_site),
-        )
+        render_pre(preconditions, function, span)
     } else {
         quote! { #function }
     }

tests/nightly/function/compile_fail/missing_check.stderr
@@ -1,10 +1,12 @@
 error[E0061]: this function takes 1 argument but 0 arguments were supplied
  --> $DIR/missing_check.rs:7:5
   |
-4 | fn foo() {}
-  | -------- defined here
+3 |   #[pre("must be bar")]
+  |  _______-
+4 | | fn foo() {}
+  | |___________- defined here
 ...
-7 |     foo()
-  |     ^^^-- supplied 0 arguments
-  |     |
-  |     expected 1 argument
+7 |       foo()
+  |       ^^^-- supplied 0 arguments
+  |       |
+  |       expected 1 argument

tests/stable/function/compile_fail/missing_check.stderr
@@ -1,10 +1,12 @@
 error[E0061]: this function takes 1 argument but 0 arguments were supplied
  --> $DIR/missing_check.rs:7:5
   |
-4 | fn foo() {}
-  | -------- defined here
+3 |   #[pre("must be bar")]
+  |  _______-
+4 | | fn foo() {}
+  | |___________- defined here
 ...
-7 |     foo()
-  |     ^^^-- supplied 0 arguments
-  |     |
-  |     expected 1 argument
+7 |       foo()
+  |       ^^^-- supplied 0 arguments
+  |       |
+  |       expected 1 argument

commit 8c76f1f9457230968d0ecd8c439380da07de1298

Fix spans for missing preconditions for unsafe
39cfecf introduced a better error message for missing preconditions.
This did not work with `unsafe` functions before this commit. It should
now work for all function signatures.

This also changed a test to track that it works for `unsafe` functions.

proc-macro/src/const_generics_impl.rs
@@ -54,7 +54,7 @@ use syn::{parse2, spanned::Spanned, Ident, ItemFn, LitStr};
 
 use crate::{
     call::Call,
-    helpers::CRATE_NAME,
+    helpers::{add_span_to_signature, CRATE_NAME},
     precondition::{Precondition, ReadWrite},
 };
 
@@ -112,12 +112,7 @@ pub(crate) fn render_pre(
 
     // Include the precondition site into the span of the function.
     // This improves the error messages for the case where no preconditions are specified.
-    function.sig.fn_token.span = function
-        .sig
-        .fn_token
-        .span
-        .join(span)
-        .unwrap_or_else(|| span);
+    add_span_to_signature(span, &mut function.sig);
 
     function.sig.inputs.push(
         parse2(quote_spanned! { span=>

proc-macro/src/helpers.rs
@@ -9,7 +9,7 @@ use syn::{
     parse::{Parse, ParseStream},
     spanned::Spanned,
     token::Paren,
-    Attribute, Expr,
+    Attribute, Expr, Signature,
 };
 
 /// The reason to display in examples on how to use reasons.
@@ -128,4 +128,27 @@ pub(crate) fn attributes_of_expression(expr: &mut Expr) -> Option<&mut Vec<Attri
         MethodCall, Paren, Path, Range, Reference, Repeat, Return, Struct, Try, TryBlock, Tuple,
         Type, Unary, Unsafe, While, Yield
     )
+}
+
+/// Incorporates the given span into the signature.
+///
+/// Ideally both are shown, when the function definition is shown.
+pub(crate) fn add_span_to_signature(span: Span, signature: &mut Signature) {
+    signature.fn_token.span = signature.fn_token.span.join(span).unwrap_or_else(|| span);
+
+    if let Some(token) = &mut signature.constness {
+        token.span = token.span.join(span).unwrap_or_else(|| span);
+    }
+
+    if let Some(token) = &mut signature.asyncness {
+        token.span = token.span.join(span).unwrap_or_else(|| span);
+    }
+
+    if let Some(token) = &mut signature.unsafety {
+        token.span = token.span.join(span).unwrap_or_else(|| span);
+    }
+
+    if let Some(abi) = &mut signature.abi {
+        abi.extern_token.span = abi.extern_token.span.join(span).unwrap_or_else(|| span);
+    }
 }

proc-macro/src/struct_impl.rs
@@ -67,6 +67,7 @@ use syn::{parse2, spanned::Spanned, Ident, ItemFn, PathArguments};
 
 use crate::{
     call::Call,
+    helpers::add_span_to_signature,
     precondition::{Precondition, ReadWrite},
 };
 
@@ -145,12 +146,7 @@ pub(crate) fn render_pre(
 
     // Include the precondition site into the span of the function.
     // This improves the error messages for the case where no preconditions are specified.
-    function.sig.fn_token.span = function
-        .sig
-        .fn_token
-        .span
-        .join(span)
-        .unwrap_or_else(|| span);
+    add_span_to_signature(span, &mut function.sig);
 
     function.sig.inputs.push(
         parse2(quote_spanned! { span=>

tests/nightly/function/compile_fail/missing_assure.rs
@@ -1,9 +1,9 @@
 use pre::pre;
 
 #[pre("is bar")]
-fn foo() {}
+unsafe fn foo() {}
 
 #[pre]
 fn main() {
-    foo()
+    unsafe { foo() }
 }

tests/nightly/function/compile_fail/missing_assure.stderr
@@ -1,12 +1,12 @@
 error[E0061]: this function takes 1 argument but 0 arguments were supplied
- --> $DIR/missing_assure.rs:8:5
+ --> $DIR/missing_assure.rs:8:14
   |
 3 |   #[pre("is bar")]
   |  _______-
-4 | | fn foo() {}
-  | |___________- defined here
+4 | | unsafe fn foo() {}
+  | |__________________- defined here
 ...
-8 |       foo()
-  |       ^^^-- supplied 0 arguments
-  |       |
-  |       expected 1 argument
+8 |       unsafe { foo() }
+  |                ^^^-- supplied 0 arguments
+  |                |
+  |                expected 1 argument

tests/stable/function/compile_fail/missing_assure.rs
@@ -1,9 +1,9 @@
 use pre::pre;
 
 #[pre("is bar")]
-fn foo() {}
+unsafe fn foo() {}
 
 #[pre]
 fn main() {
-    foo()
+    unsafe { foo() }
 }

tests/stable/function/compile_fail/missing_assure.stderr
@@ -1,12 +1,12 @@
 error[E0061]: this function takes 1 argument but 0 arguments were supplied
- --> $DIR/missing_assure.rs:8:5
+ --> $DIR/missing_assure.rs:8:14
   |
 3 |   #[pre("is bar")]
   |  _______-
-4 | | fn foo() {}
-  | |___________- defined here
+4 | | unsafe fn foo() {}
+  | |__________________- defined here
 ...
-8 |       foo()
-  |       ^^^-- supplied 0 arguments
-  |       |
-  |       expected 1 argument
+8 |       unsafe { foo() }
+  |                ^^^-- supplied 0 arguments
+  |                |
+  |                expected 1 argument

tests/templates/function/compile_fail/missing_assure.rs
@@ -1,9 +1,9 @@
 use pre::pre;
 
 #[pre("is bar")]
-fn foo() {}
+unsafe fn foo() {}
 
 #[pre]
 fn main() {
-    foo()
+    unsafe { foo() }
 }